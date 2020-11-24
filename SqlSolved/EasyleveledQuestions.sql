a98d31105fab0fda394b026f90fe0fd9e4ae5eb2
Ray <leorajat.ray@gmail.com>
2020-11-24 20:54:16 +0530

    Modified Readme

--- SqlSolved/EasyleveledQuestions.sql
+++ SqlSolved/EasyleveledQuestions.sql
@@ -212,6 +212,17 @@ Query the list of CITY names from STATION that either do not start with vowels o
 */ 
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '(^[^aeiou].*)|(.*[^aeiou]$)';
-
+/*
+PROBLEM 17 : WEATHER OBSERVATION STATION 12
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+ The STATION table is described as follows:
+|ID		|NUMBER			|
+|CITY		|VARCHAR2(21)		|
+|STATE		|VARCHAR2(2)		|
+|LAT_N		|NUMBER			|
+|LONG_W		|NUMBER			|
+*/ 
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^aeiou].*[^aeiou]$';
  
 
