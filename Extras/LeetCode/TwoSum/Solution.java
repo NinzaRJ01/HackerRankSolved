cdfeccf1b94e42d3bd2c8e585a6d385a90937c37
NinzaRJ01 <rajatagni432@gmail.com>
2021-09-21 20:00:37 +0530

    Add Solution(Leet Code) : Array > Two Sum

---
+++ Extras/LeetCode/TwoSum/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> numMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            numMap.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            // System.out.println(i);
+            if(numMap.containsKey(target-nums[i])){
+                int key = numMap.get(target-nums[i]);
+                if(key==i)continue;
+                int[] temp = {i,key};
+                return temp;
+            }
+        }
+        return new int[2];
+    }
+}
