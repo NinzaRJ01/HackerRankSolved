18982ac70f7c9811d486a88236ee750341517426
NinzaRJ01 <rajatagni432@gmail.com>
2021-12-20 23:19:32 +0530

    Added Solution(CodeChef): DSA Learning Series > Easy Problems > Sum Is EveryWhere

---
+++ Extras/CodeChef/DSALearningSeries/EasyProblems/SumIsEveryWhere/Codechef.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+//Problem : Sum Is EveryWhere
+//Problem Code : SUMEVOD
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		long n = (new Scanner(System.in)).nextLong();
+		System.out.println(n*n +" "+ ((n*n)+(n)));
+		/*
+		    for n = 10
+		    o - 1,3,5,7,9 ,11,13,15,17,19 ->
+		    e - 2,4,6,8,10 ,12,14,16,18,20
+		*/
+	}
+}
+
