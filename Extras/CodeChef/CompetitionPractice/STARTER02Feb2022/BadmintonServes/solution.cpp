9cd7adac079ab7b69d727c698832de247d9d8b2d
NinzaRJ01 <rajatagni432@gmail.com>
2022-02-06 00:09:58 +0530

    Added Solution(Starter): Badminton Severs

---
+++ Extras/CodeChef/CompetitionPractice/STARTER02Feb2022/BadmintonServes/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//Problem code : BADMINTON
+int main() {
+	int t;
+	cin>>t;
+	while(t--!=0){
+	    long int p;
+	    cin>>p;
+	    cout<<((p/2)+1)<<endl;
+	}
+	return 0;
+}
+
