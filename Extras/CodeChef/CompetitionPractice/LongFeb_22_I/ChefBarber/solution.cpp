80b942862eb6050a8dfd9404558024f564246527
NinzaRJ01 <rajatagni432@gmail.com>
2022-02-09 14:06:59 +0530

    Added Solution(Long I): Chef Barber

---
+++ Extras/CodeChef/CompetitionPractice/LongFeb_22_I/ChefBarber/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long int
+//Problem Code :CHEFBARBER
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    ll n,m;
+	    cin>>n>>m;
+	    cout<<(n*m)<<endl;
+	}
+	return 0;
+}
+
