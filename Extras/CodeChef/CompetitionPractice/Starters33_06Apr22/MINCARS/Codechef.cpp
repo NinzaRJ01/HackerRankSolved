3b998bf973ffd94b9d407b5d78ba1b1679007f5e
NinzaRJ01 <rajatagni432@gmail.com>
2022-04-08 09:36:48 +0530

    Added Solution (CodeChef): Starters 33 > Minimum Cars Required

---
+++ Extras/CodeChef/CompetitionPractice/Starters33_06Apr22/MINCARS/Codechef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int t=0;
+	cin>>t;
+	while(t--){
+	    double n = 0.0;
+	    cin>>n;
+	    cout<<int(ceil(n/4))<<endl;
+	}
+	
+	return 0;
+}
+
